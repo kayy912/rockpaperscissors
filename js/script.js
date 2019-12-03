c2582b417f8c6e27df09282b7f758469db86261e
kayy912 <kwanasiacole912@gmail.com>
2019-12-03 23:02:57 +0000

    added tie condition

--- js/script.js
+++ js/script.js
@@ -8,13 +8,26 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-$("document").ready(function){
+$("document").ready(function(){
   $("#shoot").click(function() {
     let userInput =  $("#input").val();
     let choices = ["rock","paper","scissors"];
     let computerInput = choices[Math.floor(Math.random()*3)];
-    let winnerInput
+    // let computerInput = choices[1];
+    //let winnerInput = 
+    $("#userChoice").text(`${userInput}`);
+    $("#computerChoice").text(`${computerInput}`);
+//Need to determine the winning cases + display who the winner is. 
+//Also need to personalize in CSS
+    if (userInput === computerInput) { 
+        $("#result").text("It's a tie! Play Again");
+    } else if () {
+
+    } else {
+
+    }
+
   });  
-}
+});
 
 
